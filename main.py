ca108dd509fa689f550301703e2e8a63dcdbefbc
Bryan Eckard <ch13f_48@zoho.com>
2024-01-05 15:21:26 -0500

    Working on computer choice and outcome functions

--- main.py
+++ main.py
@@ -34,14 +34,41 @@ def player_choice():
     choice = int(input("Make your choice: "))
     if choice == 0:
         print(rock)
+        """ return choice """
     elif choice == 1:
         print(paper)
+        """ return choice """
     elif choice == 2:
         print(scissors)
+        """ return choice """
     else:
         print("Invalid choice. Please enter 0 for Rock, 1 for Paper, or 2 for Scissors.")
+    return choice
+
+def computer_choice():
+    comp_choice = random.randint(0, 2)
+    print("Computer chose:\n")
+    if comp_choice == 0:
+        print(rock)
+        """ return comp_choice """
+    elif comp_choice == 1:
+        print(paper)
+        """ return comp_choice """
+    elif comp_choice == 2:
+        print(scissors)
+    return comp_choice
+
+def outcome():
+    if player_choice() == 0 and computer_choice() == 2:
+        print("You win!")
+    elif player_choice() == 0 and computer_choice() == 1:
+        print("You lose!")
+    elif player_choice() == 0 and computer_choice() == 0:
+        print("Draw!")
 
 def main():
     player_choice()
+    computer_choice()
+    outcome()
 
 main()
